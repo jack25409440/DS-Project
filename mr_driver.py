e91d59102da8c54ce89c74fa18501efc4c9739aa
Xiaohui Chen <xhchen0328@utexas.edu>
2016-11-02 21:42:09 -0700

    Split sentiment analysis and tfidf calculation

--- mr_driver.py
+++ mr_driver.py
@@ -1,13 +1,10 @@
-import sys, os
-from contextlib import contextmanager
+import sys
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from mrjob.protocol import JSONValueProtocol
 import json
 import re
 import nltk
-import numpy as np
-from textblob import TextBlob
 from math import log, fabs
 
 NUM_OF_LINES = 41135700
@@ -15,50 +12,9 @@ NUM_OF_LINES = 41135700
 
 OUTPUT_PROTOCOL = JSONValueProtocol
 
-@contextmanager
-def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
-        try:  
-            yield
-        finally:
-            sys.stdout = old_stdout
-
-def feature_extractor_by_line(line):
-    dic = {}
+def extract_text_by_line(line):
     data = json.loads(line)
-    dic['reviewId'] = data['reviewId']
-    dic['reviewerId'] = data['reviewerId']
-    dic['helpfulRate'] = get_helpful_rate(data['helpful'][0], data['helpful'][1])
-    dic['stars'] = data['stars']
-    dic.update(get_sentiments(data['reviewText']))
-    dic['reviewText'] = replace_text(data['reviewText'])
-    return data['reviewId'], dic
-
-def get_helpful_rate(nominator, denominator):
-    if denominator==0:
-        return 0.0
-    else:
-        return (nominator*1.0)/(denominator*1.0)
-
-def get_minmax(num_list):
-    returnVal = 0.0
-    for num in num_list:
-        if abs(num) - returnVal > 10E-6 : 
-            returnVal = num
-    return returnVal
-
-def get_sentiments(text):
-    blob = TextBlob(text);
-    polarities = [sentence.sentiment.polarity for sentence in blob.sentences]
-    subjectivities = [sentence.sentiment.subjectivity for sentence in blob.sentences]
-    if len(polarities) == 0:
-        polarities = [0.0]
-    if len(subjectivities) == 0:
-        subjectivities = [0.0]
-    minmax = get_minmax(polarities)
-    return {'max_min_or_max_polarity':minmax, 'magnitude_polarity':abs(minmax), 'min_polarity':min(polarities), 'max_polarity':max(polarities), 'avg_polarity':np.mean(polarities),  'min_subjectivity':min(subjectivities), 'max_subjectivity':max(subjectivities), 'avg_subjectivity':np.mean(subjectivities)}
+    return data['reviewId'], replace_text(data['reviewText'])
 
 def replace_text(text):
     text = text.lower()
@@ -80,113 +36,79 @@ def replace_text(text):
 
 class MRTfidfCalculator(MRJob):
 
-    def __init__(self, *args, **kwargs):
-        super(MRTfidfCalculator, self).__init__(*args, **kwargs)
-	with suppress_stdout():
-            nltk.download('stopwords')
-        self.stop = nltk.corpus.stopwords.words('english')
-
     def word_frequency_mapper(self, _, line):
-        docid, value = feature_extractor_by_line(line)
-        text = value.pop('reviewText', '')
+        docid, text = feature_extractor_by_line(line)
+	stopwords = nltk.corpus.stopwords.words('english')
         words = nltk.tokenize.wordpunct_tokenize(text)
         for word in words:
-            yield (word, docid), (1, value) 
+            if word in stopwords:
+                continue
+            yield (word, docid), 1 
 
-    def word_frequency_combiner(self, term_docid, values):
+    def word_frequency_combiner(self, term_docid, counts):
         total = 0
-        firstExecuted = False
-        metaJson = {}
-        for value in list(values):
-            total+=value[0]
-            if not firstExecuted:
-                metaJson = value[1]
-                firstExecuted = True
-        yield term_docid, (total, metaJson)
+        for count in counts:
+            total+=count
+        yield term_docid, total
 
 
-    def word_frequency_reducer(self, term_docid, values):
+    def word_frequency_reducer(self, term_docid, counts):
         total = 0
-        firstExecuted = False
-        metaJson = {}
-        for value in list(values):
-            total+=value[0]
-            if not firstExecuted:
-                metaJson = value[1]
-                firstExecuted = True
-        yield term_docid, (total, metaJson)
+        for count in counts:
+            total+=count
+        yield term_docid, total
 
-    def word_count_mapper(self, term_docid, value):
+
+    def word_count_mapper(self, term_docid, total):
         term, docid = term_docid
-        total, metaJson = value
-        yield docid, (term, total, metaJson)
+        yield docid, (term, total)
 
     def word_count_reducer(self, docid, values):
         d = 0;
-        term_total_metaJsons = list(values)
-        firstExecuted = False
-        metaJson = {}
-        for term_total_metaJson in term_total_metaJsons:
-            term = term_total_metaJson[0]
-            total = term_total_metaJson[1]
+        term_totals = list(values)
+        for term_total in term_totals:
+            term = term_total[0]
+            total = term_total[1]
             d += total
-            if not firstExecuted:
-                metaJson = term_total_metaJson[2]
-                firstExecuted = True
-        for term_total_metaJson in term_total_metaJsons:
-            term = term_total_metaJson[0]
-            total = term_total_metaJson[1]
-            yield (term, docid), (total, d, metaJson)
+        for term_total in term_totals:
+            term = term_totals[0]
+            total = term_totals[1]
+            yield (term, docid), (total, d)
 
-    def corpus_frequency_mapper(self, term_docid, total_d_metaJson):
+    def corpus_frequency_mapper(self, term_docid, total_d):
         term, docid = term_docid
-        total, d, metaJson = total_d_metaJson
-        yield term, (docid, total, d, 1, metaJson)
+        total, d = total_d
+        yield term, (docid, total, d)
 
-    def corpus_frequency_reducer(self, term, quadlets):
-        quadlet_list = list(quadlets)
-        n = len(quadlet_list)
-        for quadlet in quadlet_list:
-            docid = quadlet[0]
-            total = quadlet[1]
-            d = quadlet[2]
-            metaJson = quadlet[4]
-            yield (term, docid), (total, d, n, metaJson)
+    def corpus_frequency_reducer(self, term, triplets):
+        triplet_list = list(triplets)
+        n = len(triplet_list)
+        for triplet in quadlet_list:
+            docid = triplet[0]
+            total = triplet[1]
+            d = triplet[2]
+            yield (term, docid), (total, d, n)
 
-    def tfidf_mapper(self, term_docid, quadlet):
+    def tfidf_mapper(self, term_docid, triplet):
         term, docid = term_docid
-        t, d, n, metaJson  = quadlet
+        t, d, n = triplet
         tf = (t*1.0)/(d*1.0)
-        idf = log((NUM_OF_LINES*1.0)/(n*1.0))
+        idf = log((NUM_OF_LINES*1.0)/(n*1.0 + 1.0))
         tfidf = tf*idf
         key = 'tfidf_'+term
-        yield docid, ({key : tfidf}, metaJson)
+        yield docid, {key : tfidf}
 
-    def tfidf_combiner(self, docid, tuples):
-        tup_list = list(tuples)
-        dic = {}
-	firstExecuted = False
-        metaJson = {}
-        for tup in tup_list:
-            dic.update(tup[0])
-            if not firstExecuted:
-                metaJson = tup[1]
-                firstExecuted = True
+    def tfidf_combiner(self, docid, dics):
+        return_dic = {}
+        for dic in dics:
+            return_dic.update(dic)
         yield docid, (dic, metaJson)
 
     def tfidf_reducer(self, docid, tuples):
-        dic = {}
-	firstExecuted = False
-        metaJson = {}
-        for tup in list(tuples):
-            dic.update(tup[0])
-            if not firstExecuted:
-                metaJson = tup[1]
-                firstExecuted = True
-        ret_dic = metaJson.copy()
-        ret_dic.update(dic)
-        yield None, ret_dic
-
+        return_dic = {}
+        for dic in dics:
+            return_dic.update(dic)
+        yield docid, (dic, metaJson)
     
     def steps(self):
         return [
--- parser.py
+++ parser.py
@@ -12,11 +12,6 @@ from textblob import TextBlob
 import numpy as np
 import boto3
 
-def read_as_df(file_path):
-    with open(file_path, 'rU') as f:
-        data = [json.loads(row) for row in f]
-        return pd.DataFrame(data)
-
 def get_helpful_rate(nominator, denominator):
     if denominator==0:
         return 0.0
@@ -32,8 +27,20 @@ def get_minmax(num_list):
 
 def get_sentiments(text):
     blob = TextBlob(text);
-    polarities = [sentence.sentiment.polarity for sentence in blob.sentences]
-    subjectivities = [sentence.sentiment.subjectivity for sentence in blob.sentences]
+    polarities = []
+    subjectivities = []
+
+    try:
+        polarities = [sentence.sentiment.polarity for sentence in blob.sentences]
+    except AttributeError:
+        polarities = [0.0]
+	sys.stderr.write("AttributeError in getting polarities: {0}".format(text))
+    try:
+        subjectivities = [sentence.sentiment.subjectivity for sentence in blob.sentences]
+    except AttributeError:
+        subjectivities = [0.0]
+	sys.stderr.write("AttributeError in getting subjectivities: {0}".format(text))
+    
     if len(polarities) == 0:
         polarities = [0.0]
     if len(subjectivities) == 0:
@@ -41,65 +48,35 @@ def get_sentiments(text):
     minmax = get_minmax(polarities)
     return {'max_min_or_max_polarity':minmax, 'magnitude_polarity':abs(minmax), 'min_polarity':min(polarities), 'max_polarity':max(polarities), 'avg_polarity':np.mean(polarities),  'min_subjectivity':min(subjectivities), 'max_subjectivity':max(subjectivities), 'avg_subjectivity':np.mean(subjectivities)}
 
- 
-def extract_features_by_row(row):
+
+def feature_extractor_by_line(line):
     dic = {}
-    dic['reviewerId'] = row['reviewerID']
-    dic['helpfulRate'] = get_helpful_rate(row['helpful'][0], row['helpful'][1])
-    dic['stars'] = row['overall']
-    dic.update(get_sentiments(row['reviewText']))
+    data = json.loads(line)
+    reviewId = data['reviewId']
+    dic['reviewId'] = reviewId
+    dic['reviewerId'] = data['reviewerId']
+    dic['helpfulRate'] = get_helpful_rate(data['helpful'][0], data['helpful'][1])
+    dic['stars'] = data['stars']
+    dic.update(get_sentiments(data['reviewText']))
+    if reviewId % 100000 == 0:
+        print 'proceseed ', reviewId, ' records'
     return dic
-
-def extract_features(dataframe):
-    num_cores = multiprocessing.cpu_count()
-    result = Parallel(n_jobs=num_cores)(delayed(extract_features_by_row)(row) for index,row in dataframe.iterrows())
-    return pd.DataFrame.from_records(result)
-
-def save_as_csv(dataframe):
-    ts = time.time()
-    dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
-    file_path = 'amazon_review_' + dt + '.csv'
-    dataframe.to_csv(file_path)
-    return file_path
-
-def replace_text(text):
-    text = re.sub(r'\b[0-9]+(st|nd|rd|th)\b', 'orderplaceholder', text)
-    text = re.sub(r'\b1[0-9][0-9][0-9]\b', 'yearplaceholder', text)
-    text = re.sub(r'\b[0-9]+s\b','decadeplaceholder', text)
-    text = re.sub(r'\b[0-9]+(yr|yrs|year|years)\b', 'yearcountplaceholder', text)
-    text = re.sub(r'\b[0-9]+(w|wk|wks|week|weeks)\b', 'weekcountplaceholder', text)
-    text = re.sub(r'\b[0-9]+(m|month|months)\b', 'monthscountplaceholder', text)
-    text = re.sub(r'\b[0-9]+(d|day|days)\b', 'dayscountplaceholder', text)
-    text = re.sub(r'\b[0-9]+(t|ton|tons|lb|pound|pounds|kg|g|gram|grams|oz)\b', 'weightplaceholder', text)
-    text = re.sub(r'\b[0-9][a-zA-z]+', 'othercountsplaceholder', text)
-    text = re.sub(r'\b[0-9]+\b', 'numberplaceholder', text)
-    return text
-
-def tune_review_text(text_series):
-    num_cores = multiprocessing.cpu_count()
-    return Parallel(n_jobs=num_cores)(delayed(replace_text)(text) for text in text_series)
-
-def extract_tfidf_features(text_series):
-    vectorizer = TfidfVectorizer(min_df=1, use_idf=True, smooth_idf=True)
-    X = vectorizer.fit_transform(tune_review_text(text_series))
-    dataframe = pd.DataFrame(X.toarray(), columns=['tfidf_'+column for column in vectorizer.get_feature_names()])
-    dataframe.index.rename('reviewId', inplace=True)
-    return (vectorizer, dataframe)
-
+ 
 def upload_to_s3(file_path):
     s3 = boto3.resource('s3')
     data = open(file_path, 'rb')
-    s3.Bucket('amazon-review-data').put_object(Key=file_path, Body=data)
+    s3.Bucket('amazon-review-data-emr').put_object(Key=file_path, Body=data)
 
 def main(argv):
-    raw_dataframe = read_as_df(argv[0])
-    raw_dataframe.index.rename('reviewId', inplace=True)
-    transformed_dataframe = extract_features(raw_dataframe)
-    tfidf_vectorizer, tfidf_dataframe = extract_tfidf_features(raw_dataframe['reviewText'])
-    transformed_dataframe.index.rename('reviewId', inplace=True)
-    transformed_dataframe = pd.concat([transformed_dataframe, tfidf_dataframe], axis=1)
-    file_path = save_as_csv(transformed_dataframe)
-    upload_to_s3(file_path)
+    with open(argv[1], 'w+') as outfile:
+        with open(argv[0], 'rU') as infile:
+            for row in infile:
+                dic = feature_extractor_by_line(row)
+                json.dump(dic, outfile)
+                outfile.write('\n')
+    print('start uploading to s3')
+    upload_to_s3(argv[1])
+    print('finish uploading to s3')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
